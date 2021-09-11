1f2d59edd1083b46bd410e25ef2cb41b2fdfc4f0
Adnan Hashmi <hashmi.adnan791@gmail.com>
2021-09-11 22:51:04 +0530

    fixed the bug woth approved tab.

--- client/src/components/ActivityTeam/Tables/ActivitiesTotal.js
+++ client/src/components/ActivityTeam/Tables/ActivitiesTotal.js
@@ -1,31 +1,30 @@
-import React, {useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useState } from 'react';
 import AuthUserContext from '../../../Contexts/AuthUserContext';
-import {getRows}  from './ProcessData';
+import { getRows } from './ProcessData';
 import TeamDataContext from '../../../Contexts/TeamDataContext';
-import GetTable from './GetTable';
-import Filters from './Filters'
+import GetTable from './getTable';
+import Filters from './Filters';
 import RefreshContext from '../../../Contexts/RefreshContext';
 
-const CollapsibleTable = () =>{
-
+const CollapsibleTable = () => {
   //setting up props for filters
-  const [status,setStatus] = useState('')
-  const [user,setUser] = useState('');
+  const [status, setStatus] = useState('');
+  const [user, setUser] = useState('');
 
   let props = {
     status,
     setStatus,
     user,
-    setUser
-  }
+    setUser,
+  };
   let filters = {
     status,
     user,
-  }
+  };
 
   // rows has been initialized to an empty array
-  const [rows,setRows] = useState([])
+  const [rows, setRows] = useState([]);
   //this state is managed my add button component
   //whenever an update is made to the activity table, the add button toggles this state(via props)
   //then this table is renrended because use effect is listening to changes on refresh state.
@@ -35,26 +34,25 @@ const CollapsibleTable = () =>{
   // }
   //response is the data sent back by the backend server.
   //useEffect re renders everytime [authUser,refresh] is changed.
-  //this then rerenders the table and along with user's activities   
+  //this then rerenders the table and along with user's activities
 
   const authUser = useContext(AuthUserContext).authUser;
-  const {refresh,setRefresh} = useContext(AuthUserContext);
+  const { refresh, setRefresh } = useContext(AuthUserContext);
   const teamData = useContext(TeamDataContext).teamData;
 
-  useEffect(()=>{
-      // console.log(refresh)
-      if(!authUser) return
-      const res = teamData
-      setRows(getRows(res,authUser,filters));
-
-  },[authUser,refresh,status,user,teamData])                                               
+  useEffect(() => {
+    // console.log(refresh)
+    if (!authUser) return;
+    const res = teamData;
+    setRows(getRows(res, authUser, filters));
+  }, [authUser, refresh, status, user, teamData]);
 
   return (
     <div>
       {/* this is the parent component for Filters and GetTable */}
-      <Filters {...props}/>
-      <GetTable rows = {rows}/>
+      <Filters {...props} />
+      <GetTable rows={rows} />
     </div>
-  )
-}
-export default CollapsibleTable
+  );
+};
+export default CollapsibleTable;
